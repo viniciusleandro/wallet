04fa3274a0041b4197565f23603bc773df5c488d
Vinicius brodt <vini_brodt_@hotmail.com>
2023-02-15 20:35:36 -0300

    adicionado transaçoes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+import { Transaction, Wallet } from "./types"
+
+const myWallet: Wallet = {
+    Enable: true,
+    transactions: []
+}
+
+function addTransaction(transaction: Transaction){
+    myWallet.transactions.push(transaction)
+}
+
+addTransaction({description: "salario", type:"C", value: 13890})
+addTransaction({description:"lanche", type:"D", value: 200})
+addTransaction({description:"ps5", type:"D", value:4500})
+
+console.log(myWallet)
---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+import Transaction from "./Transaction";
+import Wallet from "./Wallet";
+
+export {Transaction, Wallet}
