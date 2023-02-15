cbe4b018391418bd33f66d0c06e28fcfc6fb0c0f
Vinicius brodt <vini_brodt_@hotmail.com>
2023-02-15 20:15:12 -0300

    definidos tipos da exportacao

---
+++ src/types/Transaction.ts
@@ -0,0 +1,7 @@
+interface Transaction{
+    value: number
+    description: string
+    type: "C" | "D"
+}
+
+export default Transaction
---
+++ src/types/Wallet.ts
@@ -0,0 +1,8 @@
+import Transaction from "./Transaction";
+
+interface Wallet{
+    Enable: boolean,
+    transactions: Transaction[]
+}
+
+export default Wallet
